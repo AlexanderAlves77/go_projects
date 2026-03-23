9d7955076be3027947e2c37fb8c4f40bb70c3062
Alexander Alves <vendas77.rj@gmail.com>
2026-03-23 19:12:33 -0300

    Enabling application configuration

--- quicknotes/cmd/http/main.go
+++ quicknotes/cmd/http/main.go
@@ -80,7 +80,9 @@ func noteCreate(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fmt.Println("Server running on port 5000")
+	config := loadConfig()
+
+	fmt.Printf("Server running on port %s", config.ServerPort)
 	mux := http.NewServeMux()
 
 	staticHandler := http.FileServer(http.Dir("views/static/"))
@@ -92,5 +94,5 @@ func main() {
 	mux.HandleFunc("/note/new", noteNew)
 	mux.HandleFunc("/note/create", noteCreate)
 
-	http.ListenAndServe(":5000", mux)
+	http.ListenAndServe(fmt.Sprintf(":%s", config.ServerPort), mux)
 }
