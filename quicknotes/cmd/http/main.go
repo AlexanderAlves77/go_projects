b017bfeedbe0d599e300137f222439a6a5f84d5e
Alexander Alves <vendas77.rj@gmail.com>
2026-03-24 09:47:08 -0300

    Creating a package for the HTTP handlers

--- quicknotes/cmd/http/main.go
+++ quicknotes/cmd/http/main.go
@@ -5,81 +5,9 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"text/template"
+
+	"quicknotes/internal/handlers"
 )
-
-type MyHandler struct{}
-type WorldHandler struct{}
-type HelloHandler struct{}
-
-func noteList(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	files := []string{
-		"views/templates/base.html",
-		"views/pages/home.html",
-	}
-
-	temp, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-
-	slog.Info("Executou o handler / ")
-	temp.ExecuteTemplate(w, "base", nil)
-}
-
-func noteView(w http.ResponseWriter, r *http.Request) {
-
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		http.Error(w, "Nota não encontrada", http.StatusNotFound)
-		return
-	}
-
-	files := []string{
-		"views/templates/base.html",
-		"views/pages/noteView.html",
-	}
-	temp, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-
-	temp.ExecuteTemplate(w, "base", id)
-}
-
-func noteNew(w http.ResponseWriter, r *http.Request) {
-
-	files := []string{
-		"views/templates/base.html",
-		"views/pages/noteNew.html",
-	}
-	temp, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-
-	temp.ExecuteTemplate(w, "base", nil)
-}
-
-func noteCreate(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-
-		http.Error(w, "Método não permitido", 405)
-		return
-	}
-	fmt.Fprint(w, "Criando uma nova nota...")
-}
 
 func main() {
 
@@ -95,10 +23,12 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 
-	mux.HandleFunc("/", noteList)
-	mux.HandleFunc("/note/view", noteView)
-	mux.HandleFunc("/note/new", noteNew)
-	mux.HandleFunc("/note/create", noteCreate)
+	noteHandler := handlers.NewNoteHandler()
+
+	mux.HandleFunc("/", noteHandler.NoteList)
+	mux.HandleFunc("/note/view", noteHandler.NoteView)
+	mux.HandleFunc("/note/new", noteHandler.NoteNew)
+	mux.HandleFunc("/note/create", noteHandler.NoteCreate)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", config.ServerPort), mux)
 }
