7d545a7bb59c57d366327ac59572e2215a7d6d90
Alexander Alves <vendas77.rj@gmail.com>
2026-03-25 10:31:47 -0300

    Connecting the application to the database

--- quicknotes/cmd/http/main.go
+++ quicknotes/cmd/http/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 
 	"quicknotes/internal/handlers"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 
 	config := loadConfig()
 	slog.SetDefault(newLogger(os.Stderr, config.GetLevelLog()))
+
+	dbpool, err := pgxpool.New(context.Background(), config.DBConnURL)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
+	slog.Info("The connection to the database was successful")
 
 	slog.Info(fmt.Sprintf("Server running on port %s\n", config.ServerPort))
 	mux := http.NewServeMux()
